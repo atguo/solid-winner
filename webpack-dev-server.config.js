54f186d8a66ab2f131c15b8d9a4883a2726a20ec
Hugh Wang <hghwng@gmail.com>
2016-07-02 14:55:19 +0800

    Listen to 0.0.0.0:8080.

--- webpack-dev-server.config.js
+++ webpack-dev-server.config.js
@@ -17,8 +17,8 @@ const config = {
     devtool: 'eval',
     hot: true, // Live-reload
     inline: true,
-    port: 3000, // Port Number
-    host: 'localhost', // Change to '0.0.0.0' for external facing server
+    port: 8080, // Port Number
+    host: '0.0.0.0', // Change to '0.0.0.0' for external facing server
   },
   devtool: 'eval',
   output: {
