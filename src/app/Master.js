ac8c6475a6c9df9bb61efc62cfc0922cc5e753e0
Hugh Wang <hghwng@gmail.com>
2016-07-02 18:10:54 +0800

    Add AppBar.

--- src/app/Master.js
+++ src/app/Master.js
@@ -1,12 +1,45 @@
 import React, {Component} from 'react';
+import Title from 'react-title-component'
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
 
 class Master extends Component {
+  handleTouchTapLeftIconButton() {
+    this.setState({
+      navDrawerOpen: !this.state.navDrawerOpen,
+    });
+  };
+
+
   render() {
     return (
-      <div>
-        Master:
-        {this.props.children}
-      </div>)
+      <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+        <div>
+          <Title render='Title.render' />
+
+          <AppBar
+            onLeftIconButtonTouchTap={this.handleTouchTapLeftIconButton}
+            title='AppBar.title'
+          />
+
+          Master:
+          {this.props.children}
+
+
+          <AppNavDrawer
+            style={styles.navDrawer}
+            location={location}
+            docked={docked}
+            onRequestChangeNavDrawer={this.handleChangeRequestNavDrawer}
+            onChangeList={this.handleChangeList}
+            open={navDrawerOpen}
+          />
+        </div>
+      </MuiThemeProvider>
+    )
   }
 }
 
